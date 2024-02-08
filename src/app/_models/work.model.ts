1e31023079d64526ca476aa8fde2e617d5b4a60c
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2024-02-08 11:09:10 +0100

    AGGIUSTAMENTI GRAFICI PAGINA WORKS

--- src/app/_models/work.model.ts
+++ src/app/_models/work.model.ts
@@ -2,11 +2,11 @@ export class Work {
     name: string;
     description: string;
     link: string;
-    img_link: string;
-    constructor(name: string, description:string, link:string, img_link:string){
+    github_link: string;
+    constructor(name: string, description:string, link:string, github_link:string){
         this.name = name;
         this.description = description;
         this.link = link;
-        this.img_link = img_link;
+        this.github_link = github_link;
     }
 }
--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -15,10 +15,10 @@ export class WorksComponent implements OnInit{
   works: Work[] = [];
 
   ngOnInit(){
-    this.works.push(new Work('Generic Dashboard', 'Creation of a generic dashboard template.', 'https://darling-buttercream-2e256f.netlify.app/', 'Screenshot 2023-12-15 alle 12.38.20.png'));
-    this.works.push(new Work('Zoom Reproduction', 'Reproduction of a Zoom videocall.', 'https://silver-paletas-9caf29.netlify.app/', 'Screenshot 2023-12-14 alle 10.29.55.png'));
-    this.works.push(new Work('Whatsapp Reproduction', 'Reproduction of Whatsapp using an API that response with Chuck Norris phrases.', 'https://melodious-marshmallow-b9191a.netlify.app/', 'Screenshot 2023-12-14 alle 10.25.12.png'));
-    this.works.push(new Work('Dropbox Home Page Reproduction', 'Reproduction of the Old Dropbox Website Home Page.', 'https://marvelous-genie-d025cb.netlify.app/', 'Screenshot 2023-12-14 alle 10.26.33.png'));
+    this.works.push(new Work('Generic Dashboard', 'Creation of a generic dashboard template.', 'https://darling-buttercream-2e256f.netlify.app/', 'https://github.com/NicoloCasagrande/html-css-bootstrap-dashboard'));
+    this.works.push(new Work('Zoom Reproduction', 'Reproduction of a Zoom videocall.', 'https://silver-paletas-9caf29.netlify.app/', 'https://github.com/NicoloCasagrande/html-css-zoom'));
+    this.works.push(new Work('Whatsapp Reproduction', 'Reproduction of Whatsapp using an API that response with Chuck Norris phrases.', 'https://melodious-marshmallow-b9191a.netlify.app/', 'https://github.com/NicoloCasagrande/vue-boolzapp'));
+    this.works.push(new Work('Dropbox Home Page Reproduction', 'Reproduction of the Old Dropbox Website Home Page.', 'https://marvelous-genie-d025cb.netlify.app/', 'https://github.com/NicoloCasagrande/htmlcss-dropbox'));
     
   }
 
